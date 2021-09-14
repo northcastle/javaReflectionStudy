2f45e9e78b9f5626f1d1fd63e43922a1f714dc27
wanghongchang <wanghongchang@itbeihai.com>
2021-09-14 14:31:10 +0800

    init project

---
+++ src/com/northcastle/helloworld/Helloworld.java
@@ -0,0 +1,13 @@
+package com.northcastle.helloworld;
+
+/**
+ * @Classname Helloworld
+ * @Description TODO
+ * @Date 2021/9/14 14:27
+ * @Created by northcastle
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
